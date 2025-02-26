2c39e6b992af36a40b212312beee3c79aa4009ef
Cyberpychos <adityanaidu1014@gmail.com>
2025-02-26 16:25:16 +0530

    Centred the hello hopefully ohh

--- scripts.js
+++ scripts.js
@@ -1,6 +1,4 @@
-
-
-function bottomFuction(){
+function bottomFunction(){
     let profile = document.getElementsByClassName("Profile")[0]
     if (profile){
         profile.scrollIntoView({behavior: "smooth"})
